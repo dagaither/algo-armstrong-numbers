0ecaf23953c6411c2cafccaf5386382dac910f07
David Gaither <gaither@protonmail.com>
2024-02-04 10:52:02 -0500

    Javascript complete

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -3,3 +3,29 @@
 exports.findArmstrongNumbers = function() {
 
 };
+
+function findArmstrongNumbers(numbers) {
+    const armstrongs = [];
+
+    for (const num of numbers) {
+        if (num < 10) {
+            armstrongs.push(num);
+        } 
+        
+        else {
+            const digits = num.toString().split('').map(Number);
+            const power = digits.length;
+            let sum = 0;
+
+            for (let i = 0; i < power; i++) {
+                sum += Math.pow(digits[i], power);
+            }
+
+            if (sum === num) {
+                armstrongs.push(num);
+            }
+        }
+    }
+
+    return armstrongs;
+}
