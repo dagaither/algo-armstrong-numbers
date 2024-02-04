75a6ffc5e56886efdaa91e92c02cf2e5f265e9d5
David Gaither <gaither@protonmail.com>
2024-02-04 10:20:12 -0500

    Python complete

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,21 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+
+    armstrongs = []
+
+    for num in numbers:
+
+        if num < 10:
+            armstrongs.append(num)
+
+        else:
+            digits = [int(x) for x in str(num)]
+            power = len(digits)
+            sum = 0
+
+            for i in range(power):
+                sum += digits[i]**power
+            if sum == num:
+                armstrongs.append(num)
+    
+    return armstrongs
